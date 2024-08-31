f52b0f419722deefd6669711ee7a9212d4d1f045
Penchanmonypiseth@Github.com <Penchanmonypiseth@Github.com>
2024-08-31 22:28:50 +0700

    [Update] Apply Design UI of Countries catalog

---
+++ src/shared/components/pagination/pagination.vue
@@ -0,0 +1,21 @@
+<template src="./pagination.html"></template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+});
+</script>
+<style scoped>
+@import url("./pagination.css");
+</style>
---
+++ src/views/countries-catalog/countries-catalog.vue
@@ -0,0 +1,32 @@
+<template src="./countries-catalog.html"></template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import pagination from '../../shared/components/pagination/pagination.vue'
+export default defineComponent({
+    name: "Countries-Catalog-Page",
+    components: {
+        pagination
+    },
+    data() {
+        return {
+
+        }
+    },
+    watch: {
+        //
+    },
+    mounted() {
+        //
+    },
+    computed: {
+        //
+    },
+    methods: {
+        //
+    },
+});
+
+</script>
+<style>
+@import url("./countries-catalog.css");
+</style>
